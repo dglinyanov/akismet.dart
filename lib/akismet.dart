a58d137bd50a4602a25a78b0897595e5e45afae3
Cédric Belin <cedric@belin.io>
2013-11-06 11:38:13 +0100

    Added Author class representing a comment submitter

---
+++ lib/akismet.dart
@@ -0,0 +1,8 @@
+/**
+ * Prevent comment spam using [Akismet](https://akismet.com) service.
+ */
+library akismet;
+
+part 'src/author.dart';
+//part 'src/comment.dart';
+//part 'src/service.dart';
---
+++ lib/src/author.dart
@@ -0,0 +1,27 @@
+part of akismet;
+
+/**
+ * Represents the author of a [Comment].
+ */
+class Author {
+
+  /**
+   * Creates a new comment [Author].
+   */
+  Author();
+
+  /// The author's user agent, that is the string identifying the Web browser used to submit comments.
+  String agent;
+
+  /// The author's mail address.
+  String email;
+
+  /// The author's IP address.
+  String ip;
+
+  /// The author's name.
+  String name;
+
+  /// The URL of the author's website.
+  Uri url;
+}
