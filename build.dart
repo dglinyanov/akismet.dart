bea6ed37b7e30201045d102890b501ca31a1d6f8
Cédric Belin <cedric@belin.io>
2013-11-27 04:05:39 +0100

    Integrated client script generation into Dart editor build system

--- build.dart
+++ build.dart
@@ -1,42 +1,50 @@
 #!/usr/bin/env dart
 
 /// Dart build system.
+import 'dart:convert';
 import 'dart:io';
 import 'package:args/args.dart';
 import 'package:path/path.dart' as path;
 
-/// Absolute path of the package directory.
-final String _root=path.dirname(Platform.script.toFilePath());
+/// Value indicating whether to print JSON messages to the standard output.
+bool _machineInterface=false;
 
 /// Object used to parse command line argments.
 final ArgParser _parser=new ArgParser()
   ..addOption('changed', allowMultiple: true, help: 'Specify a file that changed and should be rebuilt.')
   ..addOption('removed', allowMultiple: true, help: 'Specify a file that was removed and might affect the build.')
   ..addFlag('clean', help: 'Delete all generated files and reset any saved state.', negatable: false)
-  ..addFlag('machine', help: 'Print rich JSON messages to the standard output.', negatable: false)
   ..addFlag('full', help: 'Perform a full build.', negatable: false)
+  ..addFlag('machine', help: 'Print rich JSON messages to the standard output.', negatable: false)
+  ..addFlag('deploy', help: 'Create the files needed to deploy the application to a server.', negatable: false)
   ..addFlag('help', abbr: 'h', help: 'Print this usage information.', negatable: false);
+
+/// Absolute path of the package directory.
+final String _root=path.dirname(Platform.script.toFilePath());
+
+/// Performs a full build.
+void buildPackage() {
+  _echo('Building the package...');
+
+  var entryPoints=[ path.join(_root, 'test', 'html_test.dart') ];
+  for(final entryPoint in entryPoints) _writeScripts(entryPoint);
+}
 
 /// Deletes all generated files and reset any saved state.
 void cleanPackage() {
-  print('Cleaning the package...');
+  _echo('Cleaning the package...');
 
-  var files=[
-    '$_root/pubspec.lock',
-    '$_root/test/html.dart.js',
-    '$_root/test/html.dart.js.deps',
-    '$_root/test/html.dart.js.map',
-    '$_root/test/html.dart.precompiled.js'
-  ];
-
-  for(final path in files) {
-    var file=new File(path);
-    file.exists().then((result) {
-      if(result) {
-        print('Delete file: $path');
-        file.delete();
-      }
-    });
+  var entryPoints=[ path.join(_root, 'test', 'html_test.dart') ];
+  for(final entryPoint in entryPoints) {
+    for(final path in [ '$entryPoint.js', '$entryPoint.js.deps', '$entryPoint.js.map', '$entryPoint.precompiled.js' ]) {
+      var file=new File(path);
+      file.exists().then((exists) {
+        if(exists) {
+          _echo('Delete file: $path');
+          file.delete();
+        }
+      });
+    }
   }
 }
 
@@ -46,12 +54,15 @@ void main(List<String> arguments) {
 
   try {
     var results=_parser.parse(arguments);
+    _machineInterface=results['machine'];
+
     if(results['clean']) return cleanPackage();
+    if(results['full']) return buildPackage();
     if(results['help']) return printUsage();
   }
 
   on FormatException catch(e) {
-    print(e.message);
+    _echo(e.message, method: 'error');
     exit(1);
   }
 }
@@ -68,5 +79,50 @@ void printUsage() {
     ..writeln('Options:')
     ..write(_parser.getUsage());
 
-  print(buffer);
+  _echo(buffer);
+}
+
+/// Outputs a [message] to the console.
+///
+/// See [Dart Editor Build System](https://www.dartlang.org/tools/editor/build.html)
+/// for more information about the available parameters.
+void _echo(String message, { String method: 'info', String file: 'build.dart', int line: 0, int charStart: -1, int charEnd: -1 }) {
+  if(!_machineInterface) return print(method=='info' ? message: '[$file:$line] $message');
+
+  if(method!='info') {
+    var params={ 'file': file, 'message': message };
+    if(line>0) params['line']=line;
+    if(charStart>=0) params['charStart']=charStart;
+    if(charEnd>=0) params['charEnd']=charEnd;
+
+    var json=JSON.encode({ 'method': method, 'params': params });
+    print('[$json]');
+  }
+}
+
+/// Generates the client scripts corresponding to the specified [entryPoint] path.
+void _writeScripts(String entryPoint) {
+  if(!Platform.environment.containsKey('DART_SDK')) {
+    _echo('Unable to locate "dart2js" program: DART_SDK environment variable not set.', method: 'error');
+    exit(2);
+  }
+
+  var args=[
+    '--minify',
+    '--package-root='+path.join(_root, 'packages'),
+    '--out=$entryPoint.js',
+    entryPoint
+  ];
+
+  _echo('Run: dart2js ${args.join(' ')}');
+  Process.start(path.join(Platform.environment['DART_SDK'], 'bin', 'dart2js'), args)
+    .then((process) {
+      process.stderr.pipe(stderr);
+      process.stdout.pipe(stdout);
+      process.exitCode.then((exitCode) => exit(exitCode));
+    })
+    .catchError((error) {
+      _echo(error.toString(), method: 'error');
+      exit(3);
+    });
 }
