0f46b5636c0575d740c64c9033fd472f30fa4445
Cédric Belin <cedric@belin.io>
2013-12-04 16:51:21 +0100

    Updated the building process: added deployment command

--- build.dart
+++ build.dart
@@ -15,10 +15,10 @@ bool _machineInterface=false;
 final ArgParser _parser=new ArgParser()
   ..addOption('changed', allowMultiple: true, help: 'Specify a file that changed and should be rebuilt.')
   ..addOption('removed', allowMultiple: true, help: 'Specify a file that was removed and might affect the build.')
-  ..addFlag('clean', help: 'Delete all generated files and reset any saved state.', negatable: false)
-  ..addFlag('full', help: 'Perform a full build.', negatable: false)
   ..addFlag('machine', help: 'Print rich JSON messages to the standard output.', negatable: false)
-  ..addFlag('deploy', help: 'Create the files needed to deploy the application to a server.', negatable: false)
+  ..addFlag('clean', abbr: 'c', help: 'Delete all generated files and reset any saved state.', negatable: false)
+  ..addFlag('full', abbr: 'f', help: 'Perform a full build.', negatable: false)
+  ..addFlag('deploy', abbr: 'd', help: 'Create the files needed to deploy the application to a server.', negatable: false)
   ..addFlag('help', abbr: 'h', help: 'Print this usage information.', negatable: false);
 
 /// Absolute path of the package directory.
@@ -28,26 +28,61 @@ final String _root=path.dirname(Platform.script.toFilePath());
 void buildPackage() {
   _echo('Building the package...');
 
-  var entryPoints=[ path.join(_root, 'test', 'html_test.dart') ];
-  for(final entryPoint in entryPoints) _writeScripts(entryPoint);
+  var entryPoint=path.join(_root, 'test', 'html_test.dart');
+  _writeScripts(entryPoint, path.dirname(entryPoint), format: 'js');
 }
 
 /// Deletes all generated files and reset any saved state.
 void cleanPackage() {
   _echo('Cleaning the package...');
 
-  var entryPoints=[ path.join(_root, 'test', 'html_test.dart') ];
-  for(final entryPoint in entryPoints) {
-    for(final path in [ '$entryPoint.js', '$entryPoint.js.deps', '$entryPoint.js.map', '$entryPoint.precompiled.js' ]) {
-      var file=new File(path);
-      file.exists().then((exists) {
-        if(exists) {
-          _echo('Delete file: $path');
-          file.delete();
-        }
-      });
+  // Remove build directory.
+  var directory=new Directory(path.join(_root, 'build'));
+  if(directory.existsSync()) {
+    _echo('Delete directory: ${directory.path}');
+    directory.deleteSync(recursive: true);
+  }
+
+  // Remove minified JavaScript.
+  var entryPoint=path.join(_root, 'test', 'html_test.dart');
+  for(final asset in [ '$entryPoint.js', '$entryPoint.js.deps', '$entryPoint.js.map', '$entryPoint.precompiled.js' ]) {
+    var file=new File(asset);
+    if(file.existsSync()) {
+      _echo('Delete file: ${file.path}');
+      file.deleteSync();
     }
   }
+}
+
+/// Creates the files needed to deploy the application to a server.
+void deployPackage() {
+  _echo('Deploying the package...');
+
+  var output=new Directory(path.join(_root, 'build'));
+  if(!output.existsSync()) {
+    _echo('Create directory: ${output.path}');
+    output.createSync(recursive: true);
+  }
+
+  // Copy assets.
+  var srcFile=new File(path.join(_root, 'test', 'index.html'));
+  var dstFile=new File(path.join(output.path, path.basename(srcFile.path)));
+
+  _echo('Copy file: ${srcFile.path} => ${dstFile.path}');
+  dstFile.writeAsStringSync(srcFile.readAsStringSync());
+
+  // Generate minified JavaScript.
+  buildPackage();
+
+  var entryPoint=path.join(_root, 'test', 'html_test.dart');
+  srcFile=new File('$entryPoint.precompiled.js');
+  dstFile=new File(path.join(output.path, path.basename(entryPoint)+'.js'));
+
+  _echo('Copy file: ${srcFile.path} => ${dstFile.path}');
+  dstFile.writeAsStringSync(srcFile.readAsStringSync());
+
+  // Generate minified Dart.
+  _writeScripts(entryPoint, output.path, format: 'dart');
 }
 
 /// Starts the application using the specified command line [arguments].
@@ -59,6 +94,7 @@ void main(List<String> arguments) {
     _machineInterface=results['machine'];
 
     if(results['clean']) return cleanPackage();
+    if(results['deploy']) return deployPackage();
     if(results['full']) return buildPackage();
     if(results['help']) return printUsage();
   }
@@ -103,20 +139,34 @@ void _echo(String message, { String method: 'info', String file: 'build.dart', i
 }
 
 /// Generates the client scripts corresponding to the specified [entryPoint] path.
-void _writeScripts(String entryPoint) {
+void _writeScripts(String entryPoint, String outputDirectory, { format: 'js' }) {
   if(!Platform.environment.containsKey('DART_SDK')) {
     _echo('Unable to locate "dart2js" program: DART_SDK environment variable not set.', method: 'error');
     exit(2);
   }
 
+  var script=path.basename(entryPoint);
+  if(format!='dart') script+='.$format';
+
   var args=[
     '--minify',
     '--package-root='+path.join(_root, 'packages'),
-    '--out=$entryPoint.js',
+    '--out=$outputDirectory/$script',
     entryPoint
   ];
 
+  if(format=='dart') args.add('--output-type=dart');
   _echo('Run: dart2js ${args.join(' ')}');
+
+  var result=Process.runSync(path.join(Platform.environment['DART_SDK'], 'bin', 'dart2js'), args);
+  if(result.exitCode!=0) {
+    _echo(result.stderr.length>0 ? result.stderr : result.stdout, method: 'error');
+    exit(3);
+  }
+
+  _echo(result.stdout);
+
+  /* TODO
   Process.start(path.join(Platform.environment['DART_SDK'], 'bin', 'dart2js'), args)
     .then((process) {
       process.stderr.pipe(stderr);
@@ -126,5 +176,5 @@ void _writeScripts(String entryPoint) {
     .catchError((error) {
       _echo(error.toString(), method: 'error');
       exit(3);
-    });
+    });*/
 }
--- test/html_test.dart
+++ test/html_test.dart
@@ -3,6 +3,7 @@ library akismet.tests.html;
 
 import 'core_test.dart' as core;
 import 'dart:html';
+import 'dart:js';
 import 'package:akismet/html.dart';
 import 'package:unittest/html_enhanced_config.dart';
 
@@ -10,32 +11,27 @@ import 'package:unittest/html_enhanced_config.dart';
 void main() {
   useHtmlEnhancedConfiguration();
 
-  var apiKey=(querySelector('#apiKey') as InputElement);
-  apiKey.focus();
+  var $=context['jQuery'].apply;
+  $([ '[data-toggle="tooltip"]' ]).callMethod('tooltip');
 
-  var button=(querySelector('#runTests') as ButtonElement);
+  var button=(querySelector('#btn-submit') as ButtonElement);
   button.onClick.listen((event) {
     event.preventDefault();
 
-    var blog=(querySelector('#blog') as InputElement);
+    var blog=(querySelector('#blog-url') as InputElement);
     blog.value=blog.value.trim();
     if(blog.value.length==0) blog.value='http://akismet.belin.io';
 
-    var serviceUrl=(querySelector('#serviceUrl') as InputElement);
+    var serviceUrl=(querySelector('#service-url') as InputElement);
     serviceUrl.value=serviceUrl.value.trim();
-    if(serviceUrl.value.length==0) serviceUrl.value='http://belin.io:3019';
+    if(serviceUrl.value.length==0) serviceUrl.value='http://api.belin.io:3019';
 
+    var apiKey=(querySelector('#api-key') as InputElement);
     apiKey.value=apiKey.value.trim();
     if(apiKey.value.length==0) return window.alert('You must provide a valid Akismet API key in order to run the unit tests.');
 
     core.main();
     var client=new Client(apiKey.value, Uri.parse(blog.value))..serviceUrl=Uri.parse(serviceUrl.value);
     new core.ClientTest(client).run();
-  });
-
-  button=(querySelector('#visitHomepage') as ButtonElement);
-  button.onClick.listen((event) {
-    event.preventDefault();
-    window.location.href='https://pub.dartlang.org/packages/akismet';
   });
 }
